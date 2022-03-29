d7cc9aed711fb9977255fde59aaa2b10ea881470
Jose Faustino <josefaustino.channel@gmail.com>
2022-03-29 14:09:05 -0600

    MVP

--- Loops/ViewController.swift
+++ Loops/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+//MARK: Declare Outlets and Variables
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
 
+    
+    @IBAction func forInLoop(_ sender: Any)
+    {
+        var fruit = ["apple", "pear", "strawberry", " peach"]
+        
+        var output = ""
+        
+        for item in fruit
+        {
+            output += "\(item)\n"
+        }
+        
+        textView.text = output
+        
+        
+    }
+    
 
 }
 
